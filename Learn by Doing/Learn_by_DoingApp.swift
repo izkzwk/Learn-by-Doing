32e4797fde140b5abb8a5011874d7d21cf933074
Dzmitry Bladyka <bladykodmitry@gmail.com>
2023-04-09 17:53:02 +0300

    Initial Commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Dzmitry Bladyka on 9.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
